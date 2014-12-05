50036acdddc4ba41962e23e864cb01f8d506bdb1
JPark <park.joonw@gmail.com>
2014-12-05 16:16:09 -0700

    6.2.4 Format validation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: "asdf", email: "asdf")
+    @user = User.new(name: "asdf", email: "asdf@gmail.com")
   end
 
   test "should be valid" do
@@ -28,4 +28,25 @@ class UserTest < ActiveSupport::TestCase
     @user.email = "a" * 256
     assert_not @user.valid?
   end
+
+  test "emails that should be valid" do
+    valid_emails = %w[user@gmail.com 2@a.c E_t-@foo.bar.org 
+                    first.last@foo.jp]
+    valid_emails.each do |valid_value|
+      @user.email = valid_value
+      assert @user.valid?, "#{valid_value} is valid"
+    end
+  end
+
+  test "emails that are not valid" do
+    invalid_emails = %w[asdf_gmail.com asdf@gmail,com asdf@gmail 
+                        foo@bar_baz.com foo@bar+baz.com]
+      invalid_emails.each do |invalid_value|
+        @user.email = invalid_value
+        assert_not @user.valid?, "#{invalid_value} is not valid"
+      end
+  end                   
+
+
+
 end
