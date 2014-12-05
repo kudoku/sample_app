26a67b3edf6474c6d22cfd855f6bbbc14acba15e
JPark <park.joonw@gmail.com>
2014-12-05 15:41:34 -0700

    6.2.2 Validating presence

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,12 +1,21 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-
   def setup
-    @user = User.new(name: "Example User", email: "Example@email.com")
+    @user = User.new(name: "asdf", email: "asdf")
   end
 
-  test "should be valid object" do  
+  test "should be valid" do
     assert @user.valid?
+  end
+
+  test "name should be present" do
+    @user.name = ""
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = ""
+    assert_not @user.valid?
   end
 end
