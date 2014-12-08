29bb6b83aa4039ee0e748c43e1a7f268b66ebba6
JPark <park.joonw@gmail.com>
2014-12-08 10:50:35 -0700

    7.1.2 A Users resource

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
+
+  resources :users
   
 
   # The priority is based upon order of creation: first created -> highest priority.
